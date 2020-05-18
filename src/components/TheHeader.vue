3c37ff8dcc525ddcb879f17d1a47047b9b1939be
Daniel Hsieh <dan2y006@gmail.com>
2020-05-18 22:41:09 +0800

    isolate header as a component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="nav">
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <router-link class="navbar-brand" :to="{name:'Home'}">conduit</router-link>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Home'}">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Editor'}">
+              <i class="ion-compose"></i>&nbsp;New Post
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Settings'}">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Login'}">
+              <i class="ion-compose"></i>&nbsp;Sign in
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Profile'}">
+              <i class="ion-compose"></i>&nbsp;Profile
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" :to="{name:'Register'}">
+              <i class="ion-compose"></i>&nbsp;Sign up
+            </router-link>
+          </li>
+          <!-- <li>{{user}}</li> -->
+          <li>{{status}}</li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState("auth", ["user", "status"])
+  },
+  mathods: {
+    ...mapActions(["auth/logout"])
+  }
+};
+</script>
